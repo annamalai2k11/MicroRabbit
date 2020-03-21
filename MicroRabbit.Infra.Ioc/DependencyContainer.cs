9865527ac1de83cc079d6e1042c26e4822a93211
Annamalai <annamalai2k11@gmail.com>
2020-03-21 12:14:50 +0530

    Implemented Command, CommandHandler, Publish for Transfer in Microrservice

--- MicroRabbit.Infra.Ioc/DependencyContainer.cs
+++ MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -1,7 +1,10 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -14,6 +17,8 @@ namespace MicroRabbit.Infra.Ioc
         public static void RegisterService(IServiceCollection services)
         {
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
             services.AddTransient<IAccountService, AccountService>();
 
